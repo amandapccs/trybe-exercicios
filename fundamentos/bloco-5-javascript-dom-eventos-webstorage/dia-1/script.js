37c1f5d653b5ebfd511a9a2fd06525d76ca4db92
amandapccs <amandchen@hotmail.com>
2022-02-14 20:09:10 -0300

    Estudos do dia 1 adicionados

---
+++ fundamentos/bloco-5-javascript-dom-eventos-webstorage/dia-1/script.js
@@ -0,0 +1,15 @@
+document.querySelector("#header-container").style.backgroundColor = "green";
+
+document.querySelector(".emergency-tasks").style.background = "orange"
+
+document.querySelectorAll(".emergency-tasks h3")[0].style['background-color'] = "purple"
+
+document.querySelectorAll(".emergency-tasks h3")[1].style['background-color'] = "purple"
+
+document.querySelector(".no-emergency-tasks").style.background = "yellow"
+
+document.querySelectorAll(".no-emergency-tasks h3")[0].style['background-color'] = "black"
+
+document.querySelectorAll(".no-emergency-tasks h3")[1].style['background-color'] = "black"
+
+document.querySelector("#footer-container").style.background = "gray"
