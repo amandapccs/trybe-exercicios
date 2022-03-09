98bf4fd5d473c66028412eabde4efd59f5195188
amandapccs <amandchen@hotmail.com>
2022-03-09 10:10:58 -0300

    Alteracao no exercicio bonus

--- fundamentos/bloco-7-javascript-es6-e-testes-unitarios/dia-2/exercicioBonus.js
+++ fundamentos/bloco-7-javascript-es6-e-testes-unitarios/dia-2/exercicioBonus.js
@@ -37,3 +37,27 @@ console.log(mathStudents(allLessons))
 
 console.log(allLessons['lesson1']['materia']);
 console.log(allLessons.lesson1.materia);
+
+const getInfo = (obj, name) => {
+  const allLessons = [];
+  let allStudent = 0;
+  const array = Object.values(obj);
+  for (index in array) {
+    if (array[index].professor === name) {
+      allLessons.push(array[index].materia)
+      allStudent += array[index].numeroEstudantes;
+    }
+  }
+  return { lessons: allLessons, estudantes: allStudent };
+}
+
+const createReport = (allLessons, name) => {
+  const report = {};
+  report.professor = name;
+  Object.assign(report, getInfo(allLessons, name));
+  return report;
+}
+console.log(createReport(allLessons, 'Maria Clara'));
+
+console.log('-----')
+console.log(Object.values(allLessons))
